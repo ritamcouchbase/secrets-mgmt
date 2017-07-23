5de16f1df865695123cd0cbc50d4b75d65ad582c
ritamcouchbase <ritamcouchbase@gmail.com>
2017-07-23 10:37:03 +0530

    Update test.py

--- pytests/security/test.py
+++ pytests/security/test.py
@@ -546,7 +546,7 @@ class rbac_upgrade(UpgradeTests):
         upgrade_threads = self._async_update(upgrade_version=self.upgrade_version, servers=self.servers)
         for threads in upgrade_threads:
             threads.join()
-        self.check_cluster_compatiblity()
+        self.check_cluster_compatiblity(self.master)
         self.post_upgrade()
 
 
